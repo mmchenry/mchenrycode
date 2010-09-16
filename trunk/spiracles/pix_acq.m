0d6c6699329007ddf29a2d770412d0cedd476052
mchenrylab <mchenrylab@de6a7660-a47e-11de-8dbb-b5444cbab91d>
2010-09-16 18:17:37 +0000

    Added cross-correlation correction to stabilize image.  Also, added spir_animate.

--- trunk/spiracles/pix_acq.m
+++ trunk/spiracles/pix_acq.m
@@ -1,4 +1,4 @@
-function pixAcq(fName,pName,frRate)
+function pix_acq(fName,pName,frRate)
 % Acquires pixel intensity for a circular region of interest
 %
 % fName - filename of first file in image sequence
@@ -10,6 +10,9 @@ function pixAcq(fName,pName,frRate)
 % Header for file names
 nameHeader = 'Frame_'; 
 
+% Filename suffix
+sfx = 'TIF';
+
 % Number of points for drawing the roi
 numRoiPts = 100;
 
@@ -19,15 +22,19 @@ numDigits = 4;
 % Show the acquisition
 visSteps = 0;
 
+% Window size for x-correlation analysis
+winSize = 256;
+
+% Interval for advancing frames
+frameSkip = 1;
+
 
 %% Initialize for recording
 
 % Prompt to select image sequence
 if nargin < 2
-    [fName,pName] = uigetfile({'*.tif','tif images';...
-        '*.tiff','tiff images';'*.jpg','jpg images'}...
-        ,'Select first image in sequence');
-    if isempty(pName)
+    [fName,pName] = uigetfile(['*.' sfx ],'Select first image in sequence');
+    if pName==0
         return
     end
 end
@@ -47,7 +54,7 @@ if ~strcmp(nameHeader,fName(1:length(nameHeader)))
 end
 
 % Check number of frames in directory
-a = dir([pName filesep '*.TIF']);
+a = dir([pName filesep '*.' sfx]);
 numFrames = length(a);
 
 % Store parameter values
@@ -64,76 +71,277 @@ clear frameRate
 
 % Show first frame in figure window
 f = figure;
-set(f,'DoubleBuffer','on');
 I = imread([pName filesep a(1).name]);
 imshow(I)
 hold on
 
 % Select center point
 title('Choose center point')
-[xCntr,yCntr] = ginput(1);
-plot(xCntr,yCntr,'r+')
+[xStart,yStart] = ginput(1);
+
+% Store center for first frame
+d.xCntr = xStart;
+d.yCntr = yStart;
+
+% Plot center
+plot(d.xCntr,d.yCntr,'r+')
 
 % Define unit circle
 theta = linspace(0,2*pi,numRoiPts);
-xC = cos(theta);
-yC = sin(theta);
+d.xC = cos(theta);
+d.yC = sin(theta);
 
 % Select circular roi
 title('Choose radius')
 [xT,yT] = ginput(1);
-r = ((xCntr-xT)^2 + (yCntr-yT)^2)^0.5;
-xC = r.*xC + xCntr;
-yC = r.*yC + yCntr;
-roiI = roipoly(I,xC,yC);
+d.r = ((d.xCntr-xT)^2 + (d.yCntr-yT)^2)^0.5;
+xROI = d.r.*d.xC + d.xCntr;
+yROI = d.r.*d.yC + d.yCntr;
+
+% Binary at roi
+roiI0 = roipoly(I,xROI,yROI);
 
 % Show selected roi
-plot(xC,yC,'r-')
+plot(xROI,yROI,'r-')
 pause(0.5)
+close
 
-% Initiate black field, or close fig window
+
+clear theta
+
+
+%% Load initial image
+
+% % Define unit circle
+% theta = linspace(0,2*pi,numRoiPts);
+% xCirc = cos(theta);
+% yCirc = sin(theta);
+
+% Current frame number
+frNum = ['0001'];
+frName = [nameHeader frNum((end-numDigits+1):end) '.' sfx];
+I0 = imread([pName filesep frName]);
+
+% Interrogation window
+% winI0 = I0(floor(d.yCntr(1)-winSize/2):floor(d.yCntr(1)+winSize/2),...
+%              floor(d.xCntr(1)-winSize/2):floor(d.xCntr(1)+winSize/2));
+
+%winSize = min([size(I0,1) size(I0,2)]);
+
+winI0 = I0(ceil(size(I0,1)/2-winSize/2+1):floor(size(I0,1)/2+winSize/2-1),...
+           ceil(size(I0,2)/2-winSize/2+1):floor(size(I0,2)/2+winSize/2-1));
+         
+clear frName frNum 
+
+
+%% Track frame displacement with cross-correlation
+
 if visSteps
-    bI = uint8(zeros(size(I,1),size(I,2)));
-else
-    close
+    hf = figure;
+    set(hf,'DoubleBuffer','on')
+    pause(.1)
 end
 
-% Store info on roi
-d.roiX  = xC;
-d.roiY  = yC;
+% Maximum displacement allowed before resetting the 
+maxDisp = 10;
+
+% Check for file
+a = dir([pName filesep 'roi_data.mat']);
+
+if isempty(a)
+    
+    d.frames = 1:frameSkip:numFrames;
+    
+    % Loop through frames
+    for i = 2:length(d.frames)
+        
+        % Current frame number
+        frNum = ['000' num2str(d.frames(i))];
+        frName = [nameHeader frNum((end-numDigits+1):end) '.' sfx];
+        
+        % Read image
+        I = imread([pName filesep frName]);
+        
+        % Define roi in image
+        winI = I(ceil(size(I0,1)/2-winSize/2+1):floor(size(I0,1)/2+winSize/2-1),...
+            ceil(size(I0,2)/2-winSize/2+1):floor(size(I0,2)/2+winSize/2-1));
+        
+        % Find offset from image window pair
+        %[xOffset,yOffset,totOffset] = findOffset(winI,winI0);
+        
+        % Correlation coefficient for interrogation windows
+        cc = normxcorr2(winI,winI0);
+        
+        % Find index of max cc value
+        [max_cc, imax] = max(cc(:));
+        
+        % Find coordinates from max index
+        [ypeak, xpeak] = ind2sub(size(cc),imax(1));
+        
+        % Determine offset from coordinates
+        yOffset = ypeak-size(winI0,1);
+        xOffset = xpeak-size(winI0,2);
+        
+        % Find total displacement
+        totOffset = sqrt(xOffset^2 + yOffset^2);
+        
+        % Update center point from offset
+        d.xCntr(i) = xStart - xOffset;
+        d.yCntr(i) = yStart - yOffset;
+        
+        % Define roi coordinates
+        xROI = d.r.*d.xC + d.xCntr(i);
+        yROI = d.r.*d.yC + d.yCntr(i);
+        
+        % Define roi binary
+        roiI = roipoly(I,xROI,yROI);
+        
+        % Update status
+        disp(['Done frame ' num2str(i) ' of ' num2str(numFrames)])      
+        
+        if (totOffset > maxDisp) 
+   
+            % Reset reference image and center point
+            disp('Resetting reference . . .')
+            xStart = d.xCntr(i);
+            yStart = d.yCntr(i);
+            I0 = I;
+            winI0 = I0(ceil(size(I0,1)/2-winSize/2+1):floor(size(I0,1)/2+winSize/2-1),...
+                       ceil(size(I0,2)/2-winSize/2+1):floor(size(I0,2)/2+winSize/2-1));
+        end
+        
+        if visSteps
+            % Coordinates for cross-correlation data
+            xVals = [-(size(winI0,2)-1):1:size(winI0,2)-1];
+            yVals = [-(size(winI0,1)-1):1:size(winI0,1)-1];
+            
+            warning off
+            
+            subplot(3,3,1)
+            imshow(winI0)
+            title('base image')
+            
+            subplot(3,3,4)
+            imshow(winI)
+            title('current image')
+            
+            subplot(3,3,7)
+            h = pcolor(xVals,yVals,cc);
+            
+            axis square
+            set(h,'EdgeColor','none');
+            hold on
+            title(['Total displacement = ' num2str(totOffset)])
+            
+            plot([0 0],ylim,'k',xlim,[0 0],'k',xOffset,yOffset,'ko');
+            hold off
+            colormap jet
+            
+            subplot(3,3,[2 3 5 6 8 9])
+            imshow(I)
+            hold on
+            plot(xROI,yROI,'r-')
+            hold off
+            
+            pause(.001)
+            warning on
+        end
+        
+        % Clear variables
+        clear I frName frNum winI xOffset yOffset cc max_cc imax totOffset roiI
+        clear xROI yROI
+    end
+    
+    clear a maxDisp
+    
+    save([pName filesep 'roi_data'],'d')
+    
+else
+    
+    disp(' ')
+    disp('Loading previous roi data file . . .')
+    load([pName filesep 'roi_data'])
+    
+end
 
 
-%% Acquire data for each frame
+%% Postprocessing of roi kinematics
+
+cut_freq = 0.05;
+
+d.xCntr_f = butter_filt(d.xCntr,1,cut_freq,'low'); 
+d.yCntr_f = butter_filt(d.yCntr,1,cut_freq,'low'); 
+
+if 0
+    figure
+    subplot(2,1,1)
+    plot(d.frames,d.xCntr,'.',d.frames,d.xCntr_f,'-')
+    ylabel('x')
+    subplot(2,1,2)
+    plot(d.frames,d.xCntr,'.',d.frames,d.xCntr_f,'-')
+    ylabel('y')
+end
+
+
+%% Evaluate pixel values
+
 
 % Loop through frames
-for i = 1:numFrames 
+for i = 1:length(d.frames)
 
     % Current frame number
-    frNum = ['000' num2str(i)];
-    frName = [nameHeader frNum((end-numDigits+1):end) '.tif'];
+    frNum = ['000' num2str(d.frames(i))];
+    frName = [nameHeader frNum((end-numDigits+1):end) '.' sfx];
     
     % Read image
     I = imread([pName filesep frName]);
-
+    
+    % Define roi coordinates
+    d.xROI{i} = d.r.*d.xC + d.xCntr(i);
+    d.yROI{i} = d.r.*d.yC + d.yCntr(i);
+    
+    % Define roi binary
+    roiI = roipoly(I,d.xROI{i},d.yROI{i});
+    
     % Visualize roi analyzed
-    if visSteps
-        warning off
+    if visSteps      
+        
+        %tmp(roiI(:))=I(roiI(:));     
+        bI = uint8(255.*ones(size(I,1),size(I,2)));
         bI(roiI(:))=I(roiI(:));
-        imshow(bI);
-        title(['Frame ' num2str(i) ' of ' num2str(numFrames)])
-        pause(.1)
+        
+        warning off
+        subplot(1,2,1)
+        imshow(bI)
+        axis([min(d.xROI{i}) max(d.xROI{i}) ...
+            min(d.yROI{i}) max(d.yROI{i})])
+        
+        title(['Corrected: Frame ' num2str(d.frames(i)) ' of ' num2str(d.frames(end))]);
+            
+        bI = uint8(255.*ones(size(I,1),size(I,2)));
+        bI(roiI0(:))=I(roiI0(:));
+        
+        subplot(1,2,2)
+        imshow(bI)
+        axis([min(d.xROI{1}) max(d.xROI{1}) ...
+            min(d.yROI{1}) max(d.yROI{1})])
+        title('Uncorrected')
         warning on
+        
+        pause(.1)
+        
     else
-        disp(['Done frame ' num2str(i) ' of ' num2str(numFrames)])
+        disp(['Measuring pixel intensity: done frame ' ...
+              num2str(d.frames(i)) ' of ' num2str(d.frames(end))])
     end
     
     % Calc & store mean pixel value
+    d.pixVal(i)    = mean(double(I(roiI(:))));
     d.fileName{i}  = frName;
     d.frameNum(i)  = str2num(frNum);
-    d.pixVal(i)    = mean(double(I(roiI(:))));
     
     % Clear variables
-    clear I frName frNum
+    clear I frName frNum winI xOffset yOffset
 end
 
 % if ~visSteps
@@ -150,4 +358,158 @@ end
 %save([sFilePath filesep sFileName],'d');
 
 save([pName filesep 'pixel_data'],'d')
+
+
+
+function [xOffset,yOffset,totOffset] = findOffset(winI,winI0)
+% Find offset from image window pair
+
+% Correlation coefficient for interrogation windows
+cc = normxcorr2(winI,winI0);
+
+% Find index of max cc value
+[max_cc, imax] = max(abs(cc(:)));
+
+% Find coordinates from max index
+[ypeak, xpeak] = ind2sub(size(cc),imax(1));
+
+% Determine offset from coordinates
+yOffset = ypeak-size(winI0,1);
+xOffset = xpeak-size(winI0,2);
+
+% Find total displacement
+totOffset = sqrt(xOffset^2 + yOffset^2);
+
+if 1
+    % Coordinates for cross-correlation data
+    xVals = [-(size(winI0,2)-1):1:size(winI0,2)-1];
+    yVals = [-(size(winI0,1)-1):1:size(winI0,1)-1];
+    
+    subplot(3,1,1)
+    imshow(winI0)
+    title('base image')
+    
+    subplot(3,1,2)
+    imshow(winI)
+    title('current image')
+    
+    subplot(3,1,3)
+    h = pcolor(xVals,yVals,cc);
+    
+    axis square
+    set(h,'EdgeColor','none');
+    hold on
+    title('cross-correlation')
+    plot([0 0],ylim,'k',xlim,[0 0],'k',xOffset,yOffset,'ko');
+    hold off
+    colormap jet
+end
+
+return
+
+
+ 
+    %     % Define roi mask
+%     xCir = r.*xC + xCntr + xDisp;
+%     yCir = r.*yC + yCntr + yDisp;
+%     roiI = roipoly(I,xCir,yCir);
+%     I(roiI(:))=I(roiI(:));
+%     bI(roiI(:))=I(roiI(:));
+    
+    %roiI = [roiI(:,1)+xDisp roiI(:,1)+yDisp];
+    
+    % Visualize cross-correlation approach
+    if 0 %visSteps
+        
+        % Correct for yOffset
+        if yOffset<0
+            I_off = I(abs(yOffset)+1:end,:);
+        elseif yOffset==0
+            I_off = I;
+        else
+            I_off = I(1:end-abs(yOffset)-1,:);
+        end
+        
+        % Correct for xOffset
+        if xOffset<0
+            I_off = I_off(:,abs(xOffset)+1:end);
+        else
+            I_off = I_off(:,1:end-abs(xOffset)-1);
+        end
+        % Interrogation window
+    winIoff = I_off(floor(d.yCntr(i)-winSize/2):floor(d.yCntr(i)+winSize/2),...
+                    floor(d.xCntr(i)-winSize/2):floor(d.xCntr(i)+winSize/2));
+        
+        
+        
+        bI = uint8(mean(I(:)).*zeros(size(I,1),size(I,2)));
+        
+        
+        %figure;
+        warning off
+ 
+        subplot(2,2,1)
+        imshow(winI0)
+        title('base image')
+
+        subplot(2,2,3)
+        imshow(winI)
+        title('current image')
+        
+  
+        
+        subplot(2,2,2)
+        imshow(winIoff)
+        title('corrected current image')  
+        
+        colormap jet
+    end
+
+    
+    
+function data_filtered = butter_filt(data,sample_rate,cut_freq,type) 
+% High-pass or low-pass butterworth filter
+
+% All frequency values are in Hz.
+
+% Nyquist freq.
+Nqst = sample_rate/2;   
+
+% Calculate stopband frequency
+if strcmp(type,'high')
+    stop_freq = max([(cut_freq - Nqst/10) .01]);  
+
+elseif strcmp(type,'low')
+    stop_freq = min([(cut_freq + Nqst/10) (Nqst-.01)]); 
+ 
+end
+
+% Stopband Attenuation (dB)
+Astop = 30;
+
+% Passband Ripple (dB)
+Apass = 1;   
+
+% Normalise the cutoff freq. to the Nyquist freq
+Wp = cut_freq/Nqst;
+
+% Normalise the stoppass freq. to the Nyquist freq
+Ws = stop_freq/Nqst;
+
+% Check cutoff
+if (Wp > 1) || (Ws > 1)
+    error('Cutoff or bandpass frequencies cannot exceed the Nyquist freq')
+end
+
+% Calculate the order from the parameters using BUTTORD.
+[N,Fc] = buttord(Wp, Ws, Apass, Astop);    
+    
+% Calculate the zpk values using the BUTTER function.
+[B A] = butter(N, Fc, type);
+
+% Plot frequency reponse
+%freqz(B,A,512,sample_rate); 
+
+% Filter the data
+data_filtered   = filtfilt(B,A,data); 
 
--- trunk/spiracles/pool_spir.m
+++ trunk/spiracles/pool_spir.m
@@ -1,10 +1,301 @@
-function pool_spir(root_path)
+function pool_spir(rPath)
 % Collects data on all sequences the the root directory that have been 
-% analyzed with compare_spir.
+% analyzed with compare_spir. 
+%
+% The root directory must contain one directory for each temperature
+% investigated.  he temperature diretcories must end with 'F'
+
+%% Parameters
+
+spirNums = 2:5;
 
 
 %% Get path of data file, load data
 
 if nargin < 1
-    root_path = uigetdir(fName,'Select root directory');
+    rPath = uigetdir(pwd,'Select root directory');
+end
+
+if 1
+    
+    % Get listing of temperature directories
+    a1 = dir(rPath);
+    
+    % Initialize indices
+    idx1 = 1;
+    idx3 = 1;
+    
+    % Step through each temp
+    for i = 3:length(a1)
+        
+        % Proceed, if directory name ends in 'F'
+        if a1(i).isdir  && strcmp(a1(i).name(end),'F')
+            
+            % Get temp value from directory name
+            T(idx1).temp = 10*str2num(a1(i).name(1)) + ...
+                           10*str2num(a1(i).name(2));
+            
+            % Intialize variables to be stored for each spiracle
+            for k = 1:length(spirNums)
+                T(idx1).spir(spirNums(k)).delay = [];
+                T(idx1).spir(spirNums(k)).period = [];
+                T(idx1).R2(spirNums(k)).delay = [];
+                T(idx1).R3(spirNums(k)).delay = [];
+                T(idx1).R4(spirNums(k)).delay = [];
+                T(idx1).R5(spirNums(k)).delay = [];
+                T(idx1).R6(spirNums(k)).delay = [];
+            end
+            
+            % Get subdirectories
+            a2 = dir([rPath filesep a1(i).name]);
+            
+            % Loop through subdirectories
+            for j = 3:length(a2)
+                
+                % Store, if directory starts with 'R'
+                if a2(j).isdir && strcmp(a2(j).name(1),'R')
+                    
+                    % Check for 'include' file
+                    a3 = dir([rPath filesep a1(i).name filesep a2(j).name ...
+                        filesep 'include_in_pool.mat']);
+                    
+                    % Check for 'exclude' file
+                    a4 = dir([rPath filesep a1(i).name filesep a2(j).name ...
+                        filesep 'exclude_from_pool.mat']);
+                    
+                    % Proceed if comparison_data there
+                    if ~isempty(a3)
+                        
+                        % Load 'r' structure
+                        load([rPath filesep a1(i).name filesep a2(j).name ...
+                            filesep 'comparison_data.mat']);
+                        
+                        % Load corresponding 's' structure
+                        load([rPath filesep a1(i).name filesep a2(j).name '.mat'])
+                        
+                        % Store values in 'T' for L-R comparisons
+                        % for same spiracle number
+                        rNum_str = num2str(s.spir_num);
+                        if length(rNum_str)==1
+                            T(idx1).spir(s.spir_num).delay(end+1)  = ...
+                                mean(r.delay);
+                            
+                            T(idx1).spir(s.spir_num).period(end+1) = ...
+                                mean([r.periodM r.periodP]);
+                            
+                        elseif strcmp(rNum_str(1),'2')
+                            sNum = str2num(rNum_str(2));
+                            T(idx1).R2(sNum).delay(end+1) = ...
+                                mean(r.delay);
+                            
+                        elseif strcmp(rNum_str(2),'2')
+                            sNum = str2num(rNum_str(1));
+                            T(idx1).R2(sNum).delay(end+1) = ...
+                                mean(r.delay);
+                            
+                        elseif strcmp(rNum_str(1),'3')
+                            sNum = str2num(rNum_str(2));
+                            T(idx1).R3(sNum).delay(end+1) = ...
+                                mean(r.delay);
+                            
+                        elseif strcmp(rNum_str(2),'3')
+                            sNum = str2num(rNum_str(1));
+                            T(idx1).R3(sNum).delay(end+1) = ...
+                                mean(r.delay);
+                            
+                        elseif strcmp(rNum_str(1),'4')
+                            sNum = str2num(rNum_str(2));
+                            T(idx1).R4(sNum).delay(end+1) = ...
+                                mean(r.delay);
+                            
+                        elseif strcmp(rNum_str(2),'4')
+                            sNum = str2num(rNum_str(1));
+                            T(idx1).R4(sNum).delay(end+1) = ...
+                                mean(r.delay);
+                        
+                        elseif strcmp(rNum_str(1),'5')
+                            sNum = str2num(rNum_str(2));
+                            T(idx1).R5(sNum).delay(end+1) = ...
+                                mean(r.delay);
+                            
+                        elseif strcmp(rNum_str(2),'5')
+                            sNum = str2num(rNum_str(1));
+                            T(idx1).R5(sNum).delay(end+1) = ...
+                                mean(r.delay);
+                            
+                        elseif strcmp(rNum_str(1),'6')
+                            sNum = str2num(rNum_str(2));
+                            T(idx1).R6(sNum).delay(end+1) = ...
+                                mean(r.delay);
+                            
+                        elseif strcmp(rNum_str(2),'6')
+                            sNum = str2num(rNum_str(1));
+                            T(idx1).R6(sNum).delay(end+1) = ...
+                                mean(r.delay);
+                            
+                        end
+                        clear s r
+                        
+                    elseif isempty(a4)
+                        notRun{idx3} = ...
+                            [rPath filesep a1(i).name filesep a2(j).name];
+                        idx3 = idx3 + 1;   
+                        
+                    end
+                end
+            end
+            
+             % Iterate temperature index
+            idx1 = idx1 + 1;
+        end
+        
+       
+        
+    end
+    
+    %save([rPath filesep 'pooled_data'],'T')
+    
+    clear i j a1 a2 a3 idx1
+    
+else
+    load([rPath filesep 'pooled_data']);   
+end
+
+% Report unanalyzed
+disp(' ')
+disp('compare_spir not yet run on the following:')
+for i = 1:length(notRun)
+    disp(notRun{i})
+end
+disp(' ')
+
+
+%% Visualize results for same spiracle number
+
+for i = 1:length(T)
+   
+    for j = 1:length(spirNums)
+        delay_m(j)  = mean(T(i).spir(spirNums(j)).delay);
+        delay_s(j)  = std(T(i).spir(spirNums(j)).delay);
+        period_m(j) = mean(T(i).spir(spirNums(j)).period);
+        period_s(j) = std(T(i).spir(spirNums(j)).period);
+    end
+
+    figure;
+    
+    subplot(2,1,1)
+    errorbar(spirNums,period_m,period_s,period_s,'ko-')
+    hold on
+    xL = xlim;
+    h = fill([xL(1) xL(2) xL(2) xL(1)],[0 0 1/15 1/15],[.5 .5 .5]);
+    set(h,'EdgeColor','none')
+    set(h,'FaceAlpha',0.5)
+    set(gca,'XTick',spirNums)
+    ylabel('Period (s)')
+    xlabel('Spiracle number')
+    title(['L-R Comparison at ' num2str(T(i).temp) ' deg F'])
+    
+    subplot(2,1,2)
+    h = errorbar(spirNums,delay_m,delay_s,delay_s,'ko-');
+    set(gca,'XTick',spirNums)
+    hold on
+    xlabel('Spiracle number')
+    ylabel('Delay (s)')
+    ylim([-1 1])
+    xL = xlim;
+    h = fill([xL(1) xL(2) xL(2) xL(1)],[-1/30 -1/30 1/30 1/30],[.5 .5 .5]);
+    set(h,'EdgeColor','none')
+    set(h,'FaceAlpha',0.5)
+    hold off   
+    
+end
+
+return
+
+%% Visualize results for different spiracle numbers
+
+
+for i = 1:length(T)
+   
+    for j = 1:length(spirNums)        
+        R2delay_m(j)  = mean(T(i).R2(spirNums(j)).delay);
+        R2delay_s(j)  =  std(T(i).R2(spirNums(j)).delay);
+        R3delay_m(j)  = mean(T(i).R3(spirNums(j)).delay);
+        R3delay_s(j)  =  std(T(i).R3(spirNums(j)).delay);
+        R4delay_m(j)  = mean(T(i).R4(spirNums(j)).delay);
+        R4delay_s(j)  =  std(T(i).R4(spirNums(j)).delay);
+        R5delay_m(j)  = mean(T(i).R5(spirNums(j)).delay);
+        R5delay_s(j)  =  std(T(i).R5(spirNums(j)).delay);
+        R6delay_m(j)  = mean(T(i).R6(spirNums(j)).delay);
+        R6delay_s(j)  =  std(T(i).R6(spirNums(j)).delay);
+    end
+    
+    figure
+   
+    subplot(5,1,1)
+
+    errorbar(spirNums,R2delay_m,R2delay_s,R2delay_s,'ko-')
+    hold on
+    xlabel('Spiracle number')
+    ylabel('Delay (s)')
+    ylim([-1 1])
+    xL = xlim;
+    h = fill([xL(1) xL(2) xL(2) xL(1)],[-1/15 -1/15 1/15 1/15],[.5 .5 .5]);
+    set(h,'EdgeColor','none')
+    set(h,'FaceAlpha',0.5)
+    hold off
+    title(['Delay relative to spiracle 2 at ' num2str(T(i).temp) ' deg F'])
+    
+    subplot(5,1,2)
+    errorbar(spirNums,R3delay_m,R3delay_s,R3delay_s,'ko-')
+    hold on
+    xlabel('Spiracle number')
+    ylabel('Delay (s)')
+    ylim([-1 1])
+    xL = xlim;
+    h = fill([xL(1) xL(2) xL(2) xL(1)],[-1/15 -1/15 1/15 1/15],[.5 .5 .5]);
+    set(h,'EdgeColor','none')
+    set(h,'FaceAlpha',0.5)
+    hold off
+    title(['Delay relative to spiracle 3 at ' num2str(T(i).temp) ' deg F'])
+    
+    subplot(5,1,3)
+    errorbar(spirNums,R4delay_m,R4delay_s,R4delay_s,'ko-')
+    hold on
+    xlabel('Spiracle number')
+    ylabel('Delay (s)')
+    %ylim([-1 1])
+    xL = xlim;
+    h = fill([xL(1) xL(2) xL(2) xL(1)],[-1/15 -1/15 1/15 1/15],[.5 .5 .5]);
+    set(h,'EdgeColor','none')
+    set(h,'FaceAlpha',0.5)
+    hold off
+    title(['Delay relative to spiracle 4 at ' num2str(T(i).temp) ' deg F'])
+    
+    subplot(5,1,4)
+    errorbar(spirNums,R5delay_m,R5delay_s,R5delay_s,'ko-')
+    hold on
+    xlabel('Spiracle number')
+    ylabel('Delay (s)')
+    %ylim([-1 1])
+    xL = xlim;
+    h = fill([xL(1) xL(2) xL(2) xL(1)],[-1/15 -1/15 1/15 1/15],[.5 .5 .5]);
+    set(h,'EdgeColor','none')
+    set(h,'FaceAlpha',0.5)
+    hold off
+    title(['Delay relative to spiracle 5 at ' num2str(T(i).temp) ' deg F'])
+    
+    subplot(5,1,5)
+    errorbar(spirNums,R6delay_m,R6delay_s,R6delay_s,'ko-')
+    hold on
+    xlabel('Spiracle number')
+    ylabel('Delay (s)')
+    %ylim([-1 1])
+    xL = xlim;
+    h = fill([xL(1) xL(2) xL(2) xL(1)],[-1/15 -1/15 1/15 1/15],[.5 .5 .5]);
+    set(h,'EdgeColor','none')
+    set(h,'FaceAlpha',0.5)
+    hold off
+    title(['Delay relative to spiracle 6 at ' num2str(T(i).temp) ' deg F'])
+    
 end
