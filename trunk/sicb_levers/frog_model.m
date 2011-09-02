644241547cdd2a00cd5d9a4ad1339c83da4f65ce
mchenrylab <mchenrylab@de6a7660-a47e-11de-8dbb-b5444cbab91d>
2011-09-02 23:04:50 +0000

    Code used for sicb symposium talk

---
+++ trunk/sicb_levers/frog_model.m
@@ -0,0 +1,196 @@
+function r = frog_model(MA,b)
+% Model based on Lappin et al. (2006) 
+
+
+%% Parameter values
+
+% Global variables allow parameter values to be passed into this function
+global b_eff k_t m_eff
+
+% Relative tolerence of the simulation
+rel_tol = 1e-7;
+
+% Precision of simulation (default = 10^-5, use 10^-7 for high precision)
+maxError = 10^-7;
+
+% Maximum step size of simulation (s)
+maxStep   = 1e-5;
+
+% Maximum duration of simulation (s)
+t_max     = 40e-3;
+
+% Effective mass (kg)
+m_eff = 10.33/1000;
+
+% Spring constant (N/m)
+k_t = 581;
+
+% Initial position (m)
+x0 = 3.2e-3; % High effort
+%x0 = 2.3e-3; % Low effort
+
+% Assign defaults, if not provided
+if nargin < 2
+    
+    % Damping coefficient (Ns/m)
+    b_eff = 4.6;
+    
+    % Plot results
+    plot_results = 1;
+    
+    if nargin < 1
+        % Mechanical advantage
+        MA = 1/8;
+    end
+else
+    
+    % Plot results
+    plot_results = 0;
+    
+    % Define damping coefficient
+    b_eff = b;
+end
+
+
+%% Calculated parameters
+
+% Calculate mass of jaw
+% The paper included the mass of the muscle in its calculation of effective
+% mass, but did not report individual values for the jaw and muscle.  I
+% have therefore assumed that the jaw mass is much larger than the
+% effective mass.
+m_jaw = m_eff * (1/8)^2;
+%TODO:Revisit this calculation
+
+
+%% Run simulation
+
+% Simulation parameters
+options    = odeset('Events',@evnts,'RelTol',rel_tol,'MaxStep',maxStep);
+tspan      = [0 t_max];
+init_vals  = [x0; 0];
+
+% Run
+[t,X] = ode45(@gov_eqn,tspan,init_vals,options);
+
+% Store time
+r.t = t;
+
+% Extract total muscle position and velocity
+r.x_t = X(:,1);
+r.v_t = X(:,2);
+
+% Solve for acceleration
+r.a_t = -(b_eff.*r.v_t + k_t.*r.x_t)./m_eff;
+
+% Jaw velocity
+r.v_jaw = r.v_t ./ MA;
+
+% Jaw acceleration
+r.a_jaw = r.a_t ./ MA;
+
+clear X t
+
+% Dashpot force
+r.F_dash = r.v_t.*b_eff;
+
+% Spring force
+r.F_spring = r.x_t.*k_t;
+
+% Elastic energy
+r.E_elastic  = 0.5 .* k_t .* r.x_t.^2;
+
+% Kinetic energy
+r.E_kin = 0.5*m_jaw.*r.v_jaw.^2;
+
+% Dashpot energy
+r.E_dash = cumtrapz(abs(r.x_t),r.F_dash);
+
+% Total energy
+r.E_tot = r.E_elastic + r.E_kin + r.E_dash;
+
+% Efficiency
+r.eff = max(r.E_kin)./max(r.E_elastic);
+
+
+%% Plot results
+
+
+if plot_results
+
+subplot(4,1,1)
+plot(r.t.*1000,r.x_t.*1000,'k')
+xlabel('time (ms)')
+ylabel('Muscle position (mm)')
+
+subplot(4,1,2)
+plot(r.t.*1000,r.v_t.*1000,'k')
+hold on;plot(r.t.*1000,r.v_jaw.*1000,'r')
+xlabel('time (ms)')
+ylabel('Muscle vel (mm/s)')
+
+subplot(4,1,3)
+plot(r.t.*1000,r.a_t.*1000,'k')
+xlabel('time (ms)')
+ylabel('Muscle accel (mm/s)')
+
+subplot(4,1,4)
+plot(r.t.*1000,r.E_elastic.*1000,'k',r.t.*1000,r.E_kin.*1000,'b',...
+     r.t.*1000,r.E_dash.*1000,'r',r.t.*1000,r.E_tot.*1000,'k--')
+xlabel('time (ms)')
+ylabel('Energy (mJ)')
+legend('elastic','kinetic','dashpot','total')
+title(['Efficiency = ' num2str(round(r.eff.*100)) ' %'])
+
+end
+
+
+function dX = gov_eqn(t,X)
+% Defines the differential equations used to calculate the instananeous
+% torques acting on the striking body during a strike
+
+% Global variables allow parameter values to be passed into this function
+global b_eff k_t m_eff
+
+% Muscle position
+x  = X(1);
+
+% Contraction speed
+v = X(2);
+
+% Define output: contraction speed
+dX(1,1) = v;
+
+% Define output: contraction acceleration
+dX(2,1) = -(b_eff.*v + k_t.*x)./m_eff;
+
+
+
+function [value,isterminal,direction] = evnts(t,X)
+% Checks the status of a simulation to ensures that the geometry is
+% possible and halts a simulation when the resting length of the spring is
+% reached.
+
+% Define global variables to be used
+%global b_eff k_t m_eff
+
+% Muscle position
+x  = X(1);
+
+% Contraction speed
+v = X(2);
+
+% Halts execution of the model
+isterminal = 1;
+
+% Halt when spring at zero
+if x < .001e-3
+    value = 0;
+    direction = 0;
+else
+    value = 1;
+    direction = 1;
+end
+
+
+
---
+++ trunk/sicb_levers/locust_model.m
@@ -0,0 +1,404 @@
+function [d,p] = locust_model(p,visData,visAnimate)
+% Numerical solutions to locust leg kicking model
+% The angular position of the lever, theta, equals zero at rest
+%
+% Parmeter values ('leg' = tibia + tarsus)
+%   leg mass = 24e-6 (Bennet-Clark, 1975)
+%   leg diameter = 1.18e-3; (Bennet-Clark, 1975)
+%   leg length = 30e-3; (Bennet-Clark, 1975)
+%   k spring (estimate) = 1.2e4; (Bennet-Clark, 1975)
+%   Shortening length = 1.02e-3; (Bennet-Clark, 1975)
+%   In-lever length = 0.76e-3 (Heitler, 1974)
+%   Initial angle btwn tendon & leg = 6 deg (Heitler, 1974)
+
+if nargin < 2
+    visData    = 1;
+end
+
+if nargin < 3
+    visAnimate = 0;
+end
+
+
+%% Default parameters
+
+if nargin < 1
+    
+    % Mass (kg)
+    p.m = 21e-6;
+    
+    % Spring stiffness (N/m)
+    p.k = 0.3e4;
+    
+    % Spring stretch -- initial spring length beyond resting length (m)
+    p.L_s = 1.5e-3; % Makes and averge kick with  p.k = 0.3e4;
+    %p.L_s = 0.97e-3; % Makes an 'average' kick with k = 0.73e4;
+    %p.L_s = 1.2e-3; % Makes a 3 ms kick with k = 2e4
+    
+    % Distance btwn spring insertion and pivot (m)
+    p.hIn = 1.50*0.76e-3;
+    
+    % Diameter of leg (m)
+    p.leg_dia = 1.18e-3;
+    
+    % Length of leg (m)
+    p.leg_len = 30e-3;
+    
+    % Initial angle of lever system (rad)
+    p.theta0 = 6/180*pi;
+    
+    % Acceleration of gravity (m/s^2)
+    p.g = 0*9.81;
+    
+    % Density of air (kg m^-3)
+    p.rho = 0*1.2;
+    
+    % Dynamic viscosity of air (Pa s)
+    p.mu = 1.845e-5;
+    
+    % Drag coefficient for a cylinder (dimensionless)
+    p.Cd = 1.2;
+    
+    % Max excursion of theta (rad)
+    p.max_theta = 200/180*pi;
+end
+
+
+%% Calculated parameters
+
+% Position of base of spring
+%p.pSpring = [p.hIn.*cos(p.theta0)-p.L_s p.hIn.*sin(p.theta0)];
+%p.pSpring = [0 p.hIn.*sin(p.theta0)];
+
+
+p.xJoint = p.L_s-p.hIn.*cos(p.theta0);
+
+% Moment of inertia (kg m^2)
+%p.I = (p.m/12) * (p.leg_len^2 + p.leg_dia^2);
+p.I = (p.m/3) * (p.leg_len^2);
+
+% Moment of water inertia (kg m^2)
+p.I_water = pi/3 * p.rho * (p.leg_dia/2)^2 * p.leg_len^3;
+
+% Length of out-lever (m)
+p.hOut = sqrt(p.I/p.m);
+
+% Drag product
+p.drag_prod = (pi/4) * p.Cd * p.rho * (p.leg_dia/2) * p.leg_len^4;
+
+
+%p.hOut
+p.hIn/p.hOut
+
+%% Non-dimensionalize parameters
+
+% Constant to non-dimensionalize time
+t_const = 1/sqrt(p.k/p.m);
+
+% Specify global parameters that factor into governing equation
+global I I_water hIn hOut k xJoint g drag_prod m leg_len max_theta
+
+% Non-dimensionalize parameter values
+m         = p.m/p.m;
+I         = p.I/p.m/(p.hIn+p.hOut)^2;
+I_water   = p.I_water/p.m/(p.hIn+p.hOut)^2;
+drag_prod = p.drag_prod/p.m/(p.hIn+p.hOut)^2;
+k         = p.k/p.m*t_const^2;
+hIn       = p.hIn/(p.hIn+p.hOut);
+hOut      = p.hOut/(p.hIn+p.hOut);
+xJoint    = p.xJoint/(p.hIn+p.hOut);
+g         = p.g/(p.hIn+p.hOut)*t_const^2;
+leg_len   = p.leg_len/(p.hIn+p.hOut);
+max_theta = p.max_theta;
+
+
+%% Simulation parameters
+
+tspan      = [0 100*10^-3]./t_const;
+init_vals  = [p.theta0; 0];
+options    = odeset('Events',@evnts,'RelTol',1e-7);
+
+
+%% Run simulation 
+
+[t,X] = ode45(@gov_eqn,tspan,init_vals,options);
+
+theta  = X(:,1);
+Dtheta = X(:,2);
+
+
+% Clear variables
+clear X m k L_r hIn hOut pSpring
+
+% Add dimensions
+Dtheta = Dtheta ./ t_const;
+t      = t .* t_const;
+
+% Make sure sim ran for long enough
+if t(end)==tspan(2)
+    error('Evaluation time not set long enough')
+end 
+
+% Position of intersection of spring and lever
+pIn  = [ p.xJoint+p.hIn*cos(theta)   p.hIn*sin(theta)];
+
+% Position of center of intertia
+pOut = [ p.xJoint-p.hOut*cos(theta)   -p.hOut*sin(theta)];
+
+% Position of center of mass
+pCOM = [ p.xJoint-p.leg_len/2*cos(theta) -p.leg_len/2*sin(theta)];
+
+% Length of spring
+%L  = pIn(:,1)-p.xSpring;
+L = p.xJoint + p.hIn*cos(theta);
+
+% Kinetic energy
+E_kinetic = 0.5*(p.I+p.I_water).*Dtheta.^2;
+
+% Elastic energy
+E_potential = 0.5.*p.k.*L.^2;
+
+% Gravitational potential energy
+E_g = p.g*p.m.*(pCOM(:,2)-min(pCOM(:,2)));
+tau_g = cross([pCOM zeros(length(pCOM),1)],...
+        [zeros(length(pCOM),1) -p.m*p.g.*ones(length(pCOM),1) zeros(length(pCOM),1)]);
+%E_g   = cumtrapz(abs(theta(1)-theta),abs(tau_g(:,3)));
+
+% Drag torque
+tau_drag = -p.drag_prod .* Dtheta .* abs(Dtheta);
+
+% Drag energy
+E_drag = cumtrapz([0; cumsum(abs(diff(theta(1)-theta)))],abs(tau_drag));
+
+% Total energy
+E_tot = E_kinetic + E_potential + E_g + E_drag;
+
+% Re
+Re = p.rho * p.leg_dia .* Dtheta .* p.leg_len ./ p.mu;
+
+% Effective mechanical advantage
+EMA = sin(theta).*(p.hIn/p.hOut);
+
+% Velocity of center of inertia
+v = Dtheta.*p.hOut;
+
+clear t_const
+
+if visData
+    disp(' ')
+    disp(['max rotation = ' num2str(max(Dtheta).*180/pi/1000) ' deg/ms'])
+    disp(' ')
+    % disp(' ')
+    % disp(['vmax = ' num2str(max(v))])
+    % disp(['Prediction = ' num2str(sqrt(p.k/p.m)*p.L_s)])
+    % disp(' ')
+    iMax = find(theta==max(theta),1,'first');
+    disp(['Duration of kick = ' num2str(t(iMax)*1000) ' ms'])
+    disp(' ')
+    disp(['Range of theta = ' num2str(range(theta)*(180/pi)) ' deg'])
+    disp(' ')
+end
+
+
+%% Analytical approximation
+
+%const       = sin(20/180*pi);         
+% theta_ana =  p.L_s/p.hIn + (-p.L_s + p.hIn*p.theta0)/p.hIn ...
+%             .* cos(sqrt(const*p.k/p.m)*(p.hIn/p.hOut).*t);
+%         
+% Dtheta_ana = sqrt(const*p.k/p.m) .* (p.L_s - p.hIn.*p.theta0)/p.hOut ...
+%             .* sin(sqrt(const*p.k/p.m)*(p.hIn/p.hOut).*t);
+
+hIn_const = p.hIn*sin(30/180*pi);
+
+theta_ana =  p.L_s/hIn_const + (-p.L_s + hIn_const.*p.theta0)/hIn_const ...
+            .* cos(sqrt(p.k/p.m)*(hIn_const/p.hOut).*t);
+        
+% Dtheta_ana = sqrt(p.k/p.m) .* (p.L_s - hIn_const.*p.theta0)/p.hOut ...
+%             .* sin(sqrt(p.k/p.m)*(hIn_const/p.hOut).*t);
+
+lambda = pi/(p.hIn/p.hOut) * sqrt(p.m/p.k);
+
+Dtheta_ana = p.L_s.*sqrt(p.k/p.m)/p.hOut .* (0.5-0.5.*cos(pi.*t./lambda));
+        
+
+%% Animate simulation
+
+if visAnimate
+    f = figure;
+    set(f,'DoubleBuffer','on')
+    
+    for i = 1:length(t)
+        plot([p.pSpring(1) pIn(i,1)],[p.pSpring(2) pIn(i,2)],'r',...
+            [pIn(i,1) pOut(i,1)],[pIn(i,2) pOut(i,2)],'k');
+        title(['t = ' num2str(t(i))])
+        grid on
+        xlim([-5e-3 5e-3])
+        axis equal
+        pause(.5)
+        
+    end
+    
+    close
+end
+
+
+%% Plot results
+
+if visData
+    figure;
+    subplot(4,1,1)
+    plot(t,theta.*(180/pi),'-')
+%     hold on
+%     plot(t,theta_ana.*(180/pi),'r--')
+%     hold off
+    %grid on
+    xlabel('Time (s)')
+    ylabel('Theta (deg)')
+    %legend('numerical','analytical','Location','NorthWest')
+xlim([0 6e-3])
+ylim([0 180])
+    
+    subplot(4,1,2)
+    plot(t,Dtheta.*(180/pi/1000),'-')
+%     hold on
+%     plot(t,Dtheta_ana.*(180/pi/1000),'r--')
+%     hold off
+    xlabel('Time (s)')
+    ylabel('dTheta/dt (deg/ms)')
+    %grid on
+xlim([0 6e-3])
+ylim([0 90])
+    
+    subplot(4,1,3)
+    plot(t,v,'-')
+    xlabel('Time (s)')
+    ylabel('Speed (m/s)')
+    %grid on
+xlim([0 6e-3])
+ylim([0 40])
+    
+    subplot(4,1,4)
+    plot(t,E_kinetic.*1000,'-',t,E_potential.*1000,'r',t,E_tot.*1000,'k--')
+%     hold on
+%     plot(t,E_g.*1000,'g',t,E_drag.*1000,'m')
+%     hold off
+xlim([0 6e-3])
+    
+    %grid on
+    ylabel('Energy (mJ)')
+    xlabel('Time (s)')
+    legend('kinetic','elastic','grav.','drag','total','Location','West')
+    
+%     figure;
+%     subplot(3,1,1)
+%     plot(t,Re)
+%     ylabel('Re')
+%     xlabel('time (s)')
+%     
+%     subplot(3,1,2)
+%     plot(t,1./EMA)
+%     ylabel('1/EMA')
+%     xlabel('time (s)')
+end
+
+
+%% Return results
+
+d.t = t;
+d.p = p;
+d.theta = theta;
+d.Dtheta = Dtheta;
+d.theta_ana = theta_ana;
+d.Dtheta_ana = Dtheta_ana;
+d.v = v;
+d.EMA = EMA;
+d.L_range = range(L);
+
+if d.theta(end) >= p.max_theta
+    d.hit_max = true;
+else
+    d.hit_max = false;
+end
+
+
+function dX = gov_eqn(t,X)
+global I I_water hIn hOut k xJoint g drag_prod m leg_len
+
+% Angle of lever system
+theta  = X(1);
+
+% Rate of angular rotation of lever system
+Dtheta = X(2);
+
+% Position of intersection of spring and lever
+pIn  = [ xJoint+hIn*cos(theta)   hIn*sin(theta)];
+
+% Position of center of intertia
+pOut = [ xJoint-hOut*cos(theta)   -hOut*sin(theta)];
+
+% Position of center of mass
+pCOM = [ xJoint-leg_len/2*cos(theta) -leg_len/2*sin(theta)];
+
+% Current length of spring
+%L  = max([pIn(1)-pSpring(1) 0]);
+%L  = pIn(1)-pSpring(1);
+L = xJoint + hIn*cos(theta);
+
+% Torque generated by spring
+%tau_in = hIn*Fin.*sin(theta);
+tau_spring = cross([pIn 0],[-k*L 0 0]);
+%tau_spring = cross([0 hIn*sin(theta) 0],[-k*L 0 0]);
+
+% Torque from weight
+tau_g = cross([pCOM 0],[0 -m*g 0]);
+
+% Torque from drag
+tau_drag = -drag_prod .* Dtheta .* abs(Dtheta);
+
+% Define output: rate of rotation
+dX(1,1) = Dtheta;
+
+% Define output: rotational acceleration
+dX(2,1) = (tau_spring(3) + tau_g(3) + tau_drag)/(I + I_water);
+
+
+
+
+function [value,isterminal,direction] = evnts(t,X)
+
+% Define global variables to be used
+global xJoint hIn max_theta
+
+% Angle of lever system
+theta  = X(1);
+
+% Rate of angular rotation of lever system
+Dtheta = X(2);
+
+% Halts execution of the model
+isterminal = 1;
+
+
+% Current length of spring
+L = xJoint + hIn*cos(theta);
+
+if (theta>=max_theta) || (L<=0)
+    value = 0;
+    direction = 0;
+else
+    value = 1;
+    direction = 1;
+end
+
+% %if hIn*cos(X(1))<pSpring(1)
+% if X(2)<=0
+%     value = 0;
+%     direction = 0;
+% elseif X(1) >=max_theta
+%     value = 0;
+%     direction = 0;
+% else
+%     value = 1;
+%     direction = 1;
+% end
