58893e525439b552cce6e374ab87672323ee6682
mchenrylab <mchenrylab@de6a7660-a47e-11de-8dbb-b5444cbab91d>
2011-09-08 23:33:56 +0000

    Refined version of Nishikawa model (with effective mass) -- but not getting v_max to stay flat

--- trunk/sicb_levers/frog_model.m
+++ trunk/sicb_levers/frog_model.m
@@ -1,23 +1,8 @@
-function r = frog_model(MA,b)
+function r = frog_model(MA,b_eff)
 % Model based on Lappin et al. (2006) 
 
 
-%% Parameter values
-
-% Global variables allow parameter values to be passed into this function
-global b_eff k_t m_eff
-
-% Relative tolerence of the simulation
-rel_tol = 1e-7;
-
-% Precision of simulation (default = 10^-5, use 10^-7 for high precision)
-maxError = 10^-7;
-
-% Maximum step size of simulation (s)
-maxStep   = 1e-5;
-
-% Maximum duration of simulation (s)
-t_max     = 40e-3;
+%% Parameter values for the frog
 
 % Effective mass (kg)
 m_eff = 10.33/1000;
@@ -25,8 +10,8 @@ m_eff = 10.33/1000;
 % Spring constant (N/m)
 k_t = 581;
 
-% Initial position (m)
-x0 = 3.2e-3; % High effort
+% Initial muscle length (m)
+x0 = 3.2e-3./100; % High effort
 %x0 = 2.3e-3; % Low effort
 
 % Assign defaults, if not provided
@@ -45,11 +30,24 @@ if nargin < 2
 else
     
     % Plot results
-    plot_results = 0;
+    plot_results = 1;
     
-    % Define damping coefficient
-    b_eff = b;
 end
+
+
+%% Simulation parameters
+
+% Relative tolerence of the simulation
+rel_tol = 1e-7;
+
+% Precision of simulation (default = 10^-5, use 10^-7 for high precision)
+maxError = 10^-7;
+
+% Maximum step size of simulation (s)
+maxStep   = 1e-5;
+
+% Maximum duration of simulation (s)
+t_max     = 40e-3;
 
 
 %% Calculated parameters
@@ -59,8 +57,7 @@ end
 % mass, but did not report individual values for the jaw and muscle.  I
 % have therefore assumed that the jaw mass is much larger than the
 % effective mass.
-m_jaw = m_eff * (1/8)^2;
-%TODO:Revisit this calculation
+m_jaw = m_eff * (1/8);
 
 
 %% Run simulation
@@ -70,38 +67,48 @@ options    = odeset('Events',@evnts,'RelTol',rel_tol,'MaxStep',maxStep);
 tspan      = [0 t_max];
 init_vals  = [x0; 0];
 
+% Define global parameter values
+global p
+
+p.m_eff = m_jaw./MA;
+p.k_t   = k_t;
+p.MA    = MA;
+p.b_eff = b_eff;
+
+clear MA k_t m_jaw b_eff m_eff 
+
 % Run
 [t,X] = ode45(@gov_eqn,tspan,init_vals,options);
 
 % Store time
 r.t = t;
 
-% Extract total muscle position and velocity
+% Store total muscle position and velocity
 r.x_t = X(:,1);
 r.v_t = X(:,2);
 
-% Solve for acceleration
-r.a_t = -(b_eff.*r.v_t + k_t.*r.x_t)./m_eff;
+% Calculate acceleration
+r.a_t = -(p.b_eff.*r.v_t + p.k_t.*r.x_t)./p.m_eff;
 
 % Jaw velocity
-r.v_jaw = r.v_t ./ MA;
+r.v_jaw = r.v_t ./ p.MA;
 
 % Jaw acceleration
-r.a_jaw = r.a_t ./ MA;
+r.a_jaw = r.a_t ./ p.MA;
 
 clear X t
 
 % Dashpot force
-r.F_dash = r.v_t.*b_eff;
+r.F_dash = r.v_t.*p.b_eff;
 
 % Spring force
-r.F_spring = r.x_t.*k_t;
+r.F_spring = r.x_t.*p.k_t;
 
 % Elastic energy
-r.E_elastic  = 0.5 .* k_t .* r.x_t.^2;
+r.E_elastic  = 0.5 .* p.k_t .* r.x_t.^2;
 
 % Kinetic energy
-r.E_kin = 0.5*m_jaw.*r.v_jaw.^2;
+r.E_kin = 0.5*p.m_eff.*r.v_t.^2;
 
 % Dashpot energy
 r.E_dash = cumtrapz(abs(r.x_t),r.F_dash);
@@ -112,36 +119,40 @@ r.E_tot = r.E_elastic + r.E_kin + r.E_dash;
 % Efficiency
 r.eff = max(r.E_kin)./max(r.E_elastic);
 
+% Analytical estimate of max velocity
+r.v_t_max = sqrt(2*max(r.E_elastic)/p.m_eff);
+
 
 %% Plot results
 
 
 if plot_results
-
-subplot(4,1,1)
-plot(r.t.*1000,r.x_t.*1000,'k')
-xlabel('time (ms)')
-ylabel('Muscle position (mm)')
-
-subplot(4,1,2)
-plot(r.t.*1000,r.v_t.*1000,'k')
-hold on;plot(r.t.*1000,r.v_jaw.*1000,'r')
-xlabel('time (ms)')
-ylabel('Muscle vel (mm/s)')
-
-subplot(4,1,3)
-plot(r.t.*1000,r.a_t.*1000,'k')
-xlabel('time (ms)')
-ylabel('Muscle accel (mm/s)')
-
-subplot(4,1,4)
-plot(r.t.*1000,r.E_elastic.*1000,'k',r.t.*1000,r.E_kin.*1000,'b',...
-     r.t.*1000,r.E_dash.*1000,'r',r.t.*1000,r.E_tot.*1000,'k--')
-xlabel('time (ms)')
-ylabel('Energy (mJ)')
-legend('elastic','kinetic','dashpot','total')
-title(['Efficiency = ' num2str(round(r.eff.*100)) ' %'])
-
+    
+    figure
+    subplot(4,1,1)
+    plot(r.t.*1000,r.x_t.*1000,'k')
+    xlabel('time (ms)')
+    ylabel('Muscle position (mm)')
+    
+    subplot(4,1,2)
+    plot(r.t.*1000,r.v_t.*1000,'k')
+    %hold on;plot(r.t.*1000,r.v_jaw.*1000,'r')
+    xlabel('time (ms)')
+    ylabel('Muscle vel (mm/s)')
+    
+    subplot(4,1,3)
+    plot(r.t.*1000,r.a_t.*1000,'k')
+    xlabel('time (ms)')
+    ylabel('Muscle accel (mm/s)')
+    
+    subplot(4,1,4)
+    plot(r.t.*1000,r.E_elastic.*1000,'k',r.t.*1000,r.E_kin.*1000,'b',...
+        r.t.*1000,r.E_dash.*1000,'r',r.t.*1000,r.E_tot.*1000,'k--')
+    xlabel('time (ms)')
+    ylabel('Energy (mJ)')
+    legend('elastic','kinetic','dashpot','total')
+    title(['Efficiency = ' num2str(round(r.eff.*100)) ' %'])
+    
 end
 
 
@@ -150,7 +161,7 @@ function dX = gov_eqn(t,X)
 % torques acting on the striking body during a strike
 
 % Global variables allow parameter values to be passed into this function
-global b_eff k_t m_eff
+global p
 
 % Muscle position
 x  = X(1);
@@ -162,7 +173,7 @@ v = X(2);
 dX(1,1) = v;
 
 % Define output: contraction acceleration
-dX(2,1) = -(b_eff.*v + k_t.*x)./m_eff;
+dX(2,1) = -(p.b_eff.*v + p.k_t.*x)./p.m_eff;
 
 
 
